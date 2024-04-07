88cdaede270325634f2a36a90f7c7a7b4bf595a4
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-04-08 00:09:29 +0200

    restriction extension pour 'upload des photos

--- front/src/containers/NewBill.js
+++ front/src/containers/NewBill.js
@@ -20,6 +20,14 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    const fileExtension = fileName.split('.').pop().toLowerCase()
+
+    if (!['jpg', 'jpeg', 'png'].includes(fileExtension)) {
+      alert('Invalid file type. Please upload a jpg, jpeg or png file.')
+      e.target.value = null
+      return
+    }
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
